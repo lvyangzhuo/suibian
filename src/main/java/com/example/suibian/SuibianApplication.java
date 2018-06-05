6dcfa8874418b98fe6f4691b2f9ff5e48c320087
lvyangzhuo <lvyangzhuo@163.com>
2018-06-05 17:21:18 +0800

    create project

---
+++ src/main/java/com/example/suibian/SuibianApplication.java
@@ -0,0 +1,12 @@
+package com.example.suibian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuibianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuibianApplication.class, args);
+    }
+}
